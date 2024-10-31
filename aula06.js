0601c906c588faeaac9bc53ca79c384209f6e54d
Emily Bento <emilydanielli5@gmail.com>
2024-10-30 21:06:04 -0300

    segundo commit

---
+++ aula06.js
@@ -0,0 +1,7 @@
+let entrada;
+
+while ((entrada = prompt("Insira uma palavra ou número, caso deseje encerrar o programa digite 'sair'. ")) !== "sair") {
+    alert("O usuário inseriu " + entrada);
+}
+
+alert("Você saiu, até a próxima!");
